80da84617db3c4a370ab0ef5dd021a39f93d5b55
twleochao <leo.chao111@gmail.com>
2025-03-13 03:26:39 +0000

    ain't no way

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -35,7 +35,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination){
     vector<int> path;
-    if (distances[destination == INF]) return path;
+    if (distances[destination] == INF) return path;
 
     for (int i = destination; i != -1; i = previous[i]) {
         path.push_back(i);
